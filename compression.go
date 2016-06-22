8961f36750d4ac9aed38b60729da8f1f73db7fbf
Alex Nichol <unixpickle@gmail.com>
2016-06-22 14:13:45 -0400

    encode block sizes with compressed data
    This fixes a bug with end-of-file bits. Since the underlying algorithm works bit-by-bit, and since it had to pad the last byte with zeroes, it would sometimes add bogus data to the end of the file.

--- compression.go
+++ compression.go
@@ -1,40 +1,94 @@
 package lossless
 
-import "io"
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"io"
+)
+
+var headerByteOrder = binary.LittleEndian
+
+const bufferSize = 4096
 
 func Compress(p Predictor, input io.Reader, output io.Writer) error {
-	w := &bitWriter{W: output}
-	defer w.Flush()
 	p.Reset()
+	buffer := make([]byte, bufferSize)
 	for {
-		dist := p.Predictions()
-		var buf [1]byte
-		if _, err := input.Read(buf[:]); err == io.EOF {
+		n, readErr := input.Read(buffer)
+		if n == 0 {
+			if readErr == io.EOF {
+				return nil
+			} else if readErr != nil {
+				return readErr
+			} else {
+				continue
+			}
+		}
+
+		sizeField := uint32(n)
+		if err := binary.Write(output, headerByteOrder, &sizeField); err != nil {
+			return err
+		}
+		encoded := compressBlock(p, buffer[:n])
+		if _, err := output.Write(encoded); err != nil {
+			return err
+		}
+
+		if readErr == io.EOF {
 			return nil
-		} else if err != nil {
-			return err
+		} else if readErr != nil {
+			return readErr
 		}
-		if err := encodeByte(w, dist, buf[0]); err != nil {
-			return err
-		}
-		p.SawByte(buf[0])
 	}
 }
 
 func Decompress(p Predictor, input io.Reader, output io.Writer) error {
-	r := &bitReader{R: input}
 	p.Reset()
 	for {
-		dist := p.Predictions()
-		b, err := decodeByte(r, dist)
-		if err == io.EOF {
+		var blockSize uint32
+		if err := binary.Read(input, headerByteOrder, &blockSize); err == io.EOF {
 			return nil
 		} else if err != nil {
 			return err
 		}
-		if _, err := output.Write([]byte{b}); err != nil {
+		block, err := decompressBlock(p, input, int(blockSize))
+		if err != nil {
+			return errors.New("buffer underflow: " + err.Error())
+		}
+		if _, err := output.Write(block); err != nil {
 			return err
 		}
+	}
+}
+
+func compressBlock(p Predictor, block []byte) []byte {
+	var outBlock bytes.Buffer
+	w := &bitWriter{W: &outBlock}
+
+	for _, b := range block {
+		dist := p.Predictions()
+		encodeByte(w, dist, b)
 		p.SawByte(b)
 	}
+
+	w.Flush()
+	return outBlock.Bytes()
+}
+
+func decompressBlock(p Predictor, input io.Reader, blockLen int) ([]byte, error) {
+	outBlock := make([]byte, blockLen)
+
+	r := &bitReader{R: input}
+	for i := 0; i < blockLen; i++ {
+		dist := p.Predictions()
+		b, err := decodeByte(r, dist)
+		if err != nil {
+			return nil, err
+		}
+		outBlock[i] = b
+		p.SawByte(b)
+	}
+
+	return outBlock, nil
 }
